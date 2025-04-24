a28ac2de9696c6c4611b0b361b6401d0ffba37dc
Varun Pathak <vkvarun34.com>
2025-04-24 16:00:17 +0530

    Max sub array product

---
+++ src/MaxProductSubArray.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class MaxProductSubArray {
+
+    public static void main(String[] args) {
+        int[] items = {-1,-3,-10,0,60};
+        System.out.println(findMaxProductSubArray(items));
+    }
+
+    private static int findMaxProductSubArray(int[] items) {
+
+        int leftToRight = 1, rightToLeft = 1, result = Integer.MIN_VALUE;
+        for (int i = 0; i < items.length; i++) {
+            if(items[i] != 0){
+                leftToRight *= items[i];
+            }else {
+                leftToRight = 1;
+            }
+            int j = items.length - 1 - i;
+            if(items[j] != 0){
+                rightToLeft *= items[j];
+            }else {
+                rightToLeft = 1;
+            }
+            result = Math.max(leftToRight, Math.max(rightToLeft, result));
+        }
+        return result;
+    }
+}
