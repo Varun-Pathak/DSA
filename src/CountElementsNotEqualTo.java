f5953296f98e89bd6125a1611c861f50f5b77123
Varun Pathak <vkvarun34.com>
2025-05-02 08:13:10 +0530

    count number of numbers not equal to a given number

---
+++ src/CountElementsNotEqualTo.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class CountElementsNotEqualTo {
+
+    public static void main(String[] args) {
+        int[] items = {1,9,1,9};
+        int result = countNumbers(items, 1);
+        System.out.println(result);
+
+    }
+
+    private static int countNumbers(int[] items, int n) {
+
+        int count = 0;
+        for (int i = 0; i < items.length; i++) {
+            if(items[i] != n){
+                count++;
+            }
+        }
+        return count;
+
+    }
+}
