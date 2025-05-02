bf9e06ea06fa406bb77b27ed1dc07e67ef000e5c
Varun Pathak <vkvarun34.com>
2025-05-02 19:28:25 +0530

    Find all numbers which are greater than all numbers to their right

---
+++ src/Leaders.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Leaders {
+
+    public static void main(String[] args) {
+        int[] items = {16,17,4,3,5,2};
+        ArrayList result = findLeaders(items);
+        System.out.println(result);
+
+    }
+
+    private static ArrayList findLeaders(int[] items) {
+
+        int maxRight = items[items.length-1];
+        ArrayList<Integer> leaders = new ArrayList<>();
+        leaders.add(maxRight);
+        for (int i = items.length - 2; i >= 0; i--) {
+            if (items[i] >= maxRight){
+                leaders.add(items[i]);
+                maxRight = items[i];
+            }
+        }
+        Collections.reverse(leaders);
+        return leaders;
+    }
+}
