a1f17bbd119c02234b2cc7b23b9a0790a1d49322
Varun Pathak <vkvarun34.com>
2025-05-02 19:06:13 +0530

    Insert duplicate next to a number and return new array with original length

---
+++ src/InsertDuplicates.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class InsertDuplicates {
+
+    public static void main(String[] args) {
+        int[] items = {1,2,3,4};
+        int[] result = insertDuplicates(items,2);
+        System.out.println(Arrays.toString(result));
+
+    }
+
+    private static int[] insertDuplicates(int[] items, int num) {
+
+        int[] newArr = new int[2 * items.length];
+        int j = 0;
+        for (int i = 0; i < items.length; i++) {
+            newArr[j++] = items[i];
+            if (items[i] == num)
+                newArr[j++] = num;
+        }
+        return Arrays.copyOf(newArr, items.length);
+    }
+}
