fc3edaf481c22440ff57601885c1b3bf55a35676
Varun Pathak <vkvarun34.com>
2025-05-02 07:44:14 +0530

    Sum all digits of a number upto single digit

---
+++ src/SumOfDigitsUptoSingleDigit.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class SumOfDigitsUptoSingleDigit {
+
+    public static void main(String[] args) {
+        int[] items = {1,9,1};
+        int result = addDigits(55051);
+        //System.out.println(Arrays.toString(result));
+        System.out.println(result);
+    }
+
+    private static int addDigits(int num) {
+
+        if(num == 0)
+            return 0;
+        if(num % 9 == 0)
+            return 9;
+        return num % 9;
+
+    }
+}
