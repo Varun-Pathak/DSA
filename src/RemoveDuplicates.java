39abf2362ea35b0bcd5a544747acc4b03f00445e
Varun Pathak <vkvarun34.com>
2025-05-02 08:47:46 +0530

    remove duplicates and put distinct elements at the starting of the array

---
+++ src/RemoveDuplicates.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class RemoveDuplicates {
+
+    public static void main(String[] args) {
+        int[] items = {1,1,9,9,10};
+        int[] result = removeDuplicates(items);
+        System.out.println(Arrays.toString(result));
+
+    }
+
+    private static int[] removeDuplicates(int[] items) {
+
+        int count = 1;
+        for (int i = 1; i < items.length; i++) {
+            if(items[i] != items[i-1]){
+                items[count] = items[i];
+                count++;
+            }
+        }
+
+        return items;
+
+    }
+}
