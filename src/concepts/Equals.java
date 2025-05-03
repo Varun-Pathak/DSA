6d3c118b6270d3b82fdc48bb61b1fe46da726686
Varun Pathak <vkvarun34.com>
2025-05-03 15:20:13 +0530

    equals

---
+++ src/concepts/Equals.java
@@ -0,0 +1,12 @@
+package concepts;
+
+public class Equals {
+
+    public static void main(String[] args) {
+        String s1 = new String("Varun");
+        String s2 = new String("Varun");
+        System.out.println(s1 == s2);
+        System.out.println(s1.equals(s2));
+    }
+
+}
