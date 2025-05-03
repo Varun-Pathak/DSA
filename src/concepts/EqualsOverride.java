b20d31b8aaa11d34531c9320080b4f51109b1625
Varun Pathak <vkvarun34.com>
2025-05-03 16:01:27 +0530

    hashcode override

--- src/concepts/EqualsOverride.java
+++ src/concepts/EqualsOverride.java
@@ -1,12 +1,26 @@
 package concepts;
 
+import java.util.HashSet;
 import java.util.Objects;
 
 public class EqualsOverride {
 
+    public static void main(String[] args) {
+        Person p1 = new Person(32, "Varun");
+        Person p2 = new Person(32, "Varun");
+        HashSet<Object> persons = new HashSet<>();
+        persons.add(p1);
+        System.out.println(persons.contains(p2));
+    }
+
     public static class Person{
         private int age;
-        private int name;
+        private String name;
+
+        public Person(int age, String name) {
+            this.age = age;
+            this.name = name;
+        }
 
         @Override
         public boolean equals(Object object){
