d8a885dc6330d6d8dad16191794d0440466e1122
Varun Pathak <vkvarun34.com>
2025-05-03 15:40:09 +0530

    equals override

---
+++ src/concepts/EqualsOverride.java
@@ -0,0 +1,22 @@
+package concepts;
+
+import java.util.Objects;
+
+public class EqualsOverride {
+
+    public static class Person{
+        private int age;
+        private int name;
+
+        @Override
+        public boolean equals(Object object){
+            if (object == this)
+                return true;
+            if (object == null || this.getClass() != object.getClass())
+                return false;
+            Person person = (Person) object;
+            return this.age == person.age && Objects.equals(this.name, person.name);
+        }
+    }
+
+}
