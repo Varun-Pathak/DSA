a54ba2c9607bf3a9e56eb48114707a2ba6346ed3
Varun Pathak <vkvarun34.com>
2025-05-13 11:10:57 +0530

    DivisibleBy7

---
+++ src/Strings/DivisibleBy7.java
@@ -0,0 +1,22 @@
+package Strings;
+
+import java.util.List;
+
+public class DivisibleBy7 {
+
+    public static void main(String[] args) {
+        boolean result = isDivisibleBy7(51);
+        System.out.println(result);
+
+    }
+
+    private static boolean isDivisibleBy7(int num) {
+
+        while (num >= 10){
+            int b = num % 10;
+            int a = num / 10;
+            num = Math.abs(a - 2*b);
+        }
+        return num == 0 || num == 7;
+    }
+}
